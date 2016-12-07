11aecbb9b1357e1a9a96adf516cc2e9afd68cdca
aletiny <aletiny@mail.uc.edu>
2016-12-07 01:51:21 -0500

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -17,21 +17,9 @@ for line in sys.stdin:
         if current_word == word:
                 current_count += count
         else:
-
-for line in sys.stdin:
-        line = line.strip()
-        word,count = line.split('\t',1)
-        try:
-                count = int(count)
-        except ValueError:
-                continue
-        if current_word == word:
-                current_count += count
-        else:
                 if current_word:
                         print '%s\t%s' % (current_word, current_count)
                 current_count = count
                 current_word = word
 if current_word == word:
         print '%s\t%s' %(current_word, current_count)
-
